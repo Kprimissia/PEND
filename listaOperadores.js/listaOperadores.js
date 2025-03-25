3d003b95b95747815165d4b6155f241f7c3df91a
Kprimissia <159180445+Kprimissia@users.noreply.github.com>
2025-03-25 15:57:42 -0300

    Add files via upload

---
+++ listaOperadores.js/listaOperadores.js
@@ -0,0 +1,85 @@
+/* Exercício 1*/
+console.log("Exercício 1");
+
+{
+let x = 10;
+console.log(x);
+}
+
+/*Exercício 2*/
+console.log("Exercício 2");
+
+{
+let a = 20;
+let b = 5;
+
+console.log( a + b);
+console.log( a - b);
+console.log( a * b);
+console.log( a / b);
+console.log( a % b);
+}
+
+/*Exercício 3*/
+console.log("Exercício 3");
+
+{
+let num = -15;
+console.log(-num); 
+}
+
+/*Exercício 4*/
+console.log("Exercício 4");
+
+{
+let count = 5;
+
+console.log(count++);
+console.log(count--);
+}
+
+/*Exercício 5*/
+console.log("Exercício 5");
+
+{  x = 10;
+
+    console.log(x += 5);
+    console.log(x -= 3);
+    console.log(x *=2);
+    console.log(x /=4);
+    console.log(x %= 3);
+}
+
+/*Exercício 6*/
+console.log("Exercício 6");
+
+{
+let p = true;
+let q = false;
+
+console.log(p && q); 
+console.log(p || q);
+console.log(!p);
+console.log(!q);
+}
+
+/*Exercício 7*/
+console.log("Exercício 7");
+
+{
+console.log(0 || "Hello"); // Hello
+console.log("" && "world"); 
+console.log(null ?? "Default value"); //Default value
+console.log(undefined ?? 42); //42
+}
+
+/*Exercício 8*/
+console.log("Exercício 8");
+
+{
+let resultado = 10 + 5 * 2 > 20 && !false;
+
+console.log(resultado); // Exibe: false
+}
+
+
